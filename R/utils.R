cb02e6b8e1b843e5c8c6967f0077c337de6a09bd
Zachary M. Jones <zmj@zmjones.com>
2016-11-10 13:58:06 -0500

    default loss function for character outputs
    misclassification loss

--- R/utils.R
+++ R/utils.R
@@ -4,4 +4,6 @@ defaultLoss.numeric <- function(x, y) mean((x - y)^2)
 defaultLoss.factor <- function(x, y) mean(x != y)
 defaultLoss.integer <- function(x, y) defaultLoss.numeric(x, y)
 defaultLoss.ordered <- function(x, y) defaultLoss.numeric(x, y)
-defaultLoss.matrix <- function(x, y) apply((x - y)^2, 2, mean)
+defaultLoss.character <- function(x, y) defaultLoss.factor(x, y)
+## assumes first argument is the matrix
+defaultLoss.matrix <- function(x, y) apply(x, 2, function(z) defaultLoss(z, y))
